57500acc4d48e914032106fb43afb872195c1522
Ekaterina-polehina <135655715+Ekaterina-polehina@users.noreply.github.com>
2023-10-30 14:59:31 +0300

    Add files via upload

---
+++ news/app.py
@@ -0,0 +1,8 @@
+import redis
+
+
+red = redis.Redis(
+    host = 'redis-13820.c273.us-east-1-2.ec2.cloud.redislabs.com',
+    port = 13820,
+    password = 'P43R4eMFNAsZJt7eLugZbFO6micFkFHg'
+)
---
+++ news/scheduler.py
@@ -0,0 +1,10 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from .tasks import send_weekly_email_notifications
+
+
+def start():
+    print("Scheduler started!")
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(send_weekly_email_notifications, 'cron', day_of_week='mon', hour=8, minute=0)
+    scheduler.start()
